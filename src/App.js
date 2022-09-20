69301b33839499df4dbfd286b6e2ab31aa1927f0
Rishabh Maurya <rishabhmaurya7654@gmail.com>
2022-09-21 00:40:59 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import "./style.css"
+import QrCode from './components/QrCode';
+
+function App() {
+  return (
+    <div className="section container">
+      <QrCode />
+    </div>
+  );
+}
+
+export default App;
